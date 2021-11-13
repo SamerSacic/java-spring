d7db9c67e2f1cb52d452092dca76f63f041930b1
SamerSacic <samer.sacic@gmail.com>
2021-11-13 20:53:45 +0100

    Spring Security: Authorization Filter

--- userservice/src/main/java/com/sascode/userservice/security/SecurityConfig.java
+++ userservice/src/main/java/com/sascode/userservice/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sascode.userservice.security;
 
 import com.sascode.userservice.filter.CustomAuthenticationFilter;
+import com.sascode.userservice.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -28,10 +32,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Override Spring Security login url
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+        // Set Http authentication
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(this.authenticationManagerBean()));
+        // Disable Authorization for custom login url
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+
+//        http.authorizeRequests().antMatchers(GET, "/api/v1/user/**").hasAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(GET, "/api/v1/users/**").hasAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/v1/user/save/**").hasAuthority("ROLE_ADMIN");
+        // If this enabled authentication doesn't work
+        // http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
